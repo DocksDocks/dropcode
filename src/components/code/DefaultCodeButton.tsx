f25a7fdcb464e3a89c4c85df83ebd1d96b2ddc9b
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2023-07-17 18:12:17 -0300

    Adding "dynamic" color classes to tailwind

--- src/components/code/DefaultCodeButton.tsx
+++ src/components/code/DefaultCodeButton.tsx
@@ -9,9 +9,13 @@ interface DefaultCodeButtonProps {
 }
 
 export default function DefaultCodeButton (props: DefaultCodeButtonProps) {
+    let colorClass = '';
+    if (props.color === 'blue') colorClass = `bg-blue-500 hover:bg-blue-600`
+    if (props.color === 'emerald') colorClass = `bg-emerald-500 hover:bg-emerald-600`
+    if (props.color === 'yellow') colorClass = `bg-yellow-500 hover:bg-yellow-600`
     return (
         <button
-          className={`bg-${props.color}-500 hover:bg-${props.color}-600
+          className={`${colorClass}
           w-full space-x-1 py-4 rounded 
           flex place-content-center
           text-center sm:text-xl lg:text-3xl xl:text-4xl text-white font-semibold`}
--- src/components/sendcode/CustomizableButton.tsx
+++ src/components/sendcode/CustomizableButton.tsx
@@ -5,10 +5,14 @@ interface CustomizableButtonProps {
 }
 
 export default function CustomizableButton(props: CustomizableButtonProps) {
+    let colorClass = '';
+    if (props.color === 'blue') colorClass = `bg-blue-500 hover:bg-blue-600 dark:bg-blue-800 dark:hover:bg-blue-900`
+    if (props.color === 'red') colorClass = `bg-red-500 hover:bg-red-600 dark:bg-red-800 dark:hover:bg-red-900`
     return (
         <button
             onClick={props.onClick}
-            className={`font-bold py-3 px-14 rounded btn btn-primary bg-${props.color}-500 hover:bg-${props.color}-600 text-white dark:bg-${props.color}-800 dark:hover:bg-${props.color}-900`}
+            className={`font-bold py-3 px-14 rounded btn text-white
+            btn-primary ${colorClass}`}
         >
             {props.text}
         </button>
